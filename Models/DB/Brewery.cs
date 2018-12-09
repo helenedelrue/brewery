4f212d02a763e3f08ae1c1a93cc9838e66d9969f
Helene <helene_delrue@solventure.eu>
2018-12-09 18:53:58 +0100

    added validation on user input

--- Models/DB/Brewery.cs
+++ Models/DB/Brewery.cs
@@ -1,17 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Breweries.Models.DB
 {
     public partial class Brewery
     {
+        //ensure correct length and additional checks on user input
+        [StringLength(36)]
         public string Id { get; set; }
+
+        [StringLength(50)]
         public string Title { get; set; }
+
+        [EmailAddress]
+        [StringLength(50)]
         public string Email { get;  set; }
+
+        [Phone]
+        [StringLength(50)]
         public string Telephone { get; set; }
-      
+
         public string _website;
 
+        [Url]
+        [StringLength(50)]
         public string Website
         {
             //override get method to account for websites that don't include http:// at beginning
